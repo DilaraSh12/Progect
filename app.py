9edba315bd4742cf596cc72e85bbc91568c1c83f
Dilara <dilarasahmaeva047@gmail.com>
2024-12-12 23:34:15 +0500

    Project_commit

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import re
+from scraper import get_data
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///newflask.db'
+db = SQLAlchemy(app)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(300), nullable=False)
+    price_with_sale = db.Column(db.String(100))
+    image_url = db.Column(db.String(500))
+
+
+def extract_price(price_str):
+    if isinstance(price_str, str):
+        price = re.sub(r'\D', '', price_str)
+        return float(price) if price else 0.0
+    return 0.0
+
+def filter_products(min=None, max=None):
+    products = Product.query.all()
+    filtered_products = []
+    for product in products:
+        price = extract_price(product.price_with_sale)
+        if (min is None or price >= min) and (max is None or price <= max):
+            filtered_products.append(product)
+
+    return filtered_products
+
+@app.route("/")
+def index():
+    min_price = request.args.get('min_price', type=float, default=None)
+    max_price = request.args.get('max_price', type=float, default=None)
+    if min_price is not None or max_price is not None:
+        products = filter_products(min=min_price, max=max_price)
+    else:
+        products = Product.query.all()
+    return render_template('index.html', products=products)
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == '__main__':
+    with app.app_context():
+        get_data("https://www.sushifuji.ru/")
+    app.run(debug=True)
---
+++ scraper.py
@@ -0,0 +1,35 @@
+import requests
+import time
+import random
+from bs4 import BeautifulSoup
+
+def save_product_to_db(name, price_with_sale, image_url):
+    product = db.session.query(Product).filter_by(name=name).first()
+    if not product:
+        product = Product(name=name, price_with_sale=price_with_sale, image_url=image_url)
+        db.session.add(product)
+    else:
+        product.price_with_sale = price_with_sale
+    db.session.commit()
+
+
+def get_data(url):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    }
+
+    scr = requests.get(url, headers=headers)
+    soup = BeautifulSoup(scr.content, "lxml")
+    articles = soup.find_all("div", class_="col-md-4 col-lg-3 col-6 one_product_col filter-work")
+
+    for article in articles:
+        name = article.find("div", class_="text_1").span.text.strip()
+        image_url = article.find("picture").find("img")["data-src"]
+        price_with_sale = article.find("div", class_="with_sale").text.strip()
+
+        save_product_to_db(name, price_with_sale, image_url)
+
+    time.sleep(random.randint(1, 5))
+
+if __name__ == "__main__":
+    get_data("https://www.sushifuji.ru/")
